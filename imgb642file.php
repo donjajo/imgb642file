30692e4b696e398806ba5fea0e9527d8218772a7
James John <me@donjajo.com>
2021-05-30 22:41:06 +0100

    Change of License to GPLv3
    WordPress plugin directory requires plugin licenses to be GPL
    Also added extra plugin headers

--- imgb642file.php
+++ imgb642file.php
@@ -1,11 +1,16 @@
 <?php
 /**
- * Plugin Name:	ImageBase642File
- * Author:	James John
- * Author URI:	https://www.linkedin.com/in/donjajo/
- * Description:	An over-engineered-memory-safe search and replace base64 inline images to an uploaded URL when post is saved
- * Version:	1.0
- * Text Domain:	imgb642file
+ * Plugin Name:		ImageBase642File
+ * Author:		James John
+ * Author URI:		https://www.linkedin.com/in/donjajo/
+ * Description:		An over-engineered-memory-safe search and replace base64 inline images to an uploaded URL when post is saved
+ * Version:		1.0
+ * Plugin URI:		https://github.com/donjajo/imgb642file
+ * Text Domain:		imgb642file
+ * License:		GPLv3
+ * License URI:		https://www.gnu.org/licenses/gpl-3.0.html
+ * Requires PHP:	7.1
+ * Requires at least:	5.1
  */
 
 defined( 'IMGB642FILE_ABSPATH' )  || define( 'IMGB642FILE_ABSPATH', __DIR__ );
